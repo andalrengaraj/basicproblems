38de61571e44dc886dd01bb3f1f125c8373c3496
andalrengaraj <andalsanthini07@gmail.com>
2020-11-23 20:30:08 +0530

    Adding two num code

---
+++ basic two num.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double perimeter = call_perimeter(r);
+            Console.WriteLine(perimeter);
+
+
+        }
+      static public double call_perimeter(double r)
+
+       {
+          return 2*Math.PI*r;
+
+       }
+    }
+}
