b9745f07727b88cfa61cbcd30aa6f17073310fcd
andalrengaraj <andalsanthini07@gmail.com>
2020-12-07 13:38:37 +0530

    add new code

---
+++ myodd_even.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_even
+{
+    public  class myodd_even
+    {
+        static void Main(string[] args)
+        {
+            
+           
+           
+           
+        }
+    }
+}
