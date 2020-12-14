3682c172060af342be708cca8a7df4e35dc4a0eb
andalrengaraj <andalsanthini07@gmail.com>
2020-12-14 22:43:20 +0530

    Rps souce testcase file

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,337 +1,211 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using testcase_challenge;
+using rock_S;
 
-namespace UnitTestProject1
+namespace rpstestcase
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void sum_test_1()
+        public void winner_msg_1()
         {
-            //arrange
-            int a= 2147483647;
-            int b = 2147483647;
-            long  expected =4294967294;
+            choice user =choice.Rock;
+            choice  ai = choice.Rock;
+            string expected = "same choice.what a coincidence.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
 
-            //act
-            long actual = Program.call_sum(a, b);
-            //assert
+
+        }
+        [TestMethod]
+        public void winner_msg_2()
+        {
+            choice user = choice.Rock;
+            choice ai = choice.paper;
+            string expected = "paper cover Rock.sopaper wins.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void winner_msg_3()
+        {
+            choice user = choice.paper;
+            choice ai = choice.Rock;
+            string expected = "paper covers Rock.so paper wins.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void winner_msg_4()
+        {
+            choice user = choice.Rock;
+            choice ai = choice.Scissors;
+            string expected = "Rock breaks Scissors.so Rock wins.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void winner_msg_5()
+        {
+            choice user = choice.Scissors;
+            choice ai = choice.paper;
+            string expected = "paper cuts Scissors.so paper wins.";
+            string actual = Program.getwinningmessage(user,ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void winner_msg_6()
+        {
+            choice user = choice.Scissors;
+            choice ai = choice.Scissors;
+            string expected = "same choice.what a coincidence.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void winner_msg_7()
+        {
+            choice user = choice.paper;
+            choice ai = choice.Scissors;
+            string expected = "Scissors cuts paper.soScissors wins.";
+            string actual = Program.getwinningmessage(user, ai);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod]
+        public void Ai_input()
+        {
+            int i = 0;
+            while (i != 1000)
+            {
+
+                choice a = (choice)Program.getAIinput();
+                if (a != choice.paper || a != choice.Rock || a != choice.Scissors)
+                {
+                    Console.WriteLine("Worng choice other than rps");
+                }
+                else
+                {
+                    Console.WriteLine("ok");
+                }
+                i++;
+            }
+
+
+
+
+
+
+
+        }
+
+        [TestMethod]
+        public void get_winner_1()
+        {
+            choice a = choice.paper;
+            choice b = choice.paper;
+            winningstate actual = Program.getwinner(a,b);
+            winningstate expected = winningstate.draw;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void sum_test_2()
+        public void get_winner_2()
         {
-            //arrange
-            int a = -2147483647;
-            int b = -2147483647;
-            long expected = -4294967294;
-
-            //act
-            long actual = Program.call_sum(a, b);
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [TestMethod]
-        public void diff_test_1()
-        {
-            //arrange
-            int a = 2147483647;
-            int b = 2147483647;
-            long expected = 0;
-
-            //act
-            long actual = Program.call_diff(a, b);
-            //assert
+            choice a = choice.Rock;
+            choice b = choice.paper;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.AIwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void diff_test_2()
+        public void get_winner_3()
         {
-            //arrange
-            int a = int.MinValue;
-            int b = int.MaxValue;
-            long expected =- 4294967295;
-
-            //act
-            long actual = Program.call_diff(a, b);
-            //assert
+            choice a = choice.Scissors;
+            choice b = choice.paper;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.userwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void product_test_1()
+        public void get_winner_4()
         {
-            //arrange
-            int a = 2147483647;
-            int b = 2;
-            long expected =4294967294;
-
-            //act
-            long actual = Program.call_product(a, b);
-            //assert
+            choice a = choice.paper;
+            choice b = choice.Scissors;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.AIwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void diff_test_3()
+        public void get_winner_5()
         {
-            //arrange
-            int a = 0;
-            int b = 2147483647;
-            long expected =-2147483647 ;
-
-            //act
-            long actual = Program.call_diff(a, b);
-            //assert
+            choice a = choice.Rock;
+            choice b = choice.Scissors;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.userwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void diff_test_4()
+        public void get_winner_6()
         {
-            //arrange
-            int a = -2147483648;
-            int b = 2147483647;
-            long expected = -4294967295;
-
-            //act
-            long actual = Program.call_diff(a, b);
-            //assert
+            choice a = choice.Scissors;
+            choice b = choice.Rock;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.AIwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void power_test_1()
+        public void get_winner_7()
         {
-            //arrange
-            int a = 2147483647;
-            int b = 2;
-            double expected = 4.611686e18;
-
-            //act
-            double actual = Program.call_power1(a, b);
-            //assert
+            choice a = choice.paper;
+            choice b = choice.Rock;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.userwins;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void power_test_2()
+        public void get_winner_8()
         {
-            //arrange
-            int a = 0;
-            int b = 0;
-            long expected = 1;
-
-            //act
-            double actual = Program.call_power1(a, b);
-            //assert
+            choice a = choice.Rock;
+            choice b = choice.Rock;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.draw;
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void power_test_3()
+        public void get_winner_9()
         {
-            //arrange
-            int a = 0;
-            int b = 1;
-            long expected = 0;
-
-            //act
-            double actual = Program.call_power1(a, b);
-            //assert
+            choice a = choice.Scissors;
+            choice b = choice.Scissors;
+            winningstate actual = Program.getwinner(a, b);
+            winningstate expected = winningstate.draw;
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void power_test_4()
-        {
-            //arrange
-            int a = 10000;
-            int b = 10;
-            long expected =0;
 
-            //act
-            double actual = Program.call_power_2(a, b);
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
         [TestMethod]
-        public void vol_of_tank_1()
+        public void game_result_display_1()
         {
-            //arrange
-            int a = 10;
-            int b = 0;
-            int c = 12;
-            string expected = "0";
 
-            //act
-            string actual = Program.call_volue_of_tank(a, b,c);
-            //assert
-            Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void vol_of_tank_2()
-        {
-            //arrange
-            int a = int.MaxValue;
-            int b = 100;
-            int c = 12;
-            string expected ="2576980376400";
 
-            //act
-            string actual = Program.call_volue_of_tank(a, b, c);
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void vol_of_tank_3()
-        {
-            //arrange
-            int a = -1000;
-            int b = 1092;
-            int c = 12;
-            string expected = "0";
 
-            //act
-            string actual = Program.call_volue_of_tank(a, b, c);
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void vol_of_tank_4()
-        {
-            //arrange
-            double  a =10.23;
-            double  b = 1092;
-            double  c = 12.23;
-            string expected = "136623.2868";
 
-            //act
-            string actual = Program.call_volue_of_tank(a, b, c);
-            
-            
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void height_1()
-        {
-            double height = 162.3;
-            double act = 5.2626;
-           double expected = Program.call_height(height);
-           double expect= System.Math.Round(expected, 4);
 
-            Assert.AreEqual(expect,act);
-        }
-        [TestMethod]
-        public void height_2()
-        {
-            double height = 162.3;
-            double act = 63.8976;
-            double expected = Program.call_height1(height);
-            double expect = (double)System.Math.Round(expected,4);
-           
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void height_3()
-        {
-            double height = 2938;
-            double act = 0;
-            double expected = Program.call_height1(height);
-            
-
-            Assert.AreEqual(expected, act);
-        }
-        [TestMethod]
-        public void bmi_1()
-        {
-            double height = 0;
-            double weight = 0;
-            double act = 0;
-            double expected = Program.call_BMI(height, weight);
-            Assert.AreEqual(expected, act);
-        }
-        [TestMethod]
-        public void bmi_2()
-        {
-            double height = 1.79;
-            double weight = 70;
-            double act = 21.8;
-            double expected = Program.call_BMI(height, weight);
-            double expect = System.Math.Round(expected, 1);
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void bmi_3()
-        {
-            double height = 3.25893758;
-            double weight = 387398;
-            double act = 0;
-            double expected = Program.call_BMI(height, weight);
-            double expect = System.Math.Round(expected, 1);
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void bmi_4()
-        {
-            double height = 3.25;
-            double weight = 10;
-            double act = 0.9;
-            double expected = Program.call_BMI(height, weight);
-            double expect = System.Math.Round(expected, 1);
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void avera_1()
-        {
-            double n1 = 2536;
-            double n2 = 0;
-            double n3 = 2928;
-            double expect = Program.call_total(n1, n2, n3);
-            double act = 5464;
-            Assert.AreEqual(expect, act);
-        }
+          
+        
        
-        [TestMethod]
-        public void avera_3()
-        {
-            double n1 = 0;
-            double n2 = 0;
-            double n3 = 2928;
-            double expect = Program.call_total_1(n1, n2, n3);
-            double act = 2928;
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void avera_4()
-        {
-            double n1 = 0;
-            double n2 = 0;
-            double n3 = -1;
-            double expect = Program.call_total_1(n1, n2, n3);
-            double act = 0;
-            Assert.AreEqual(expect, act);
-        }
-        [TestMethod]
-        public void total_1()
-        {
-            double n1 = 100000000;
-            double n2 = 1000000000000000;
-            double n3 = 125628;
-            long expect = Program.call_total(n1, n2, n3);
-            long act = 1000000100125628;
-            Assert.AreEqual(expect, act);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+  
 }
