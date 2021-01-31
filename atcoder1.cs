e4b6470cb764fbba4a9d26c72d886876b16b990e
andalrengaraj <andalsanthini07@gmail.com>
2021-01-31 22:01:27 +0530

    Atcoder1

---
+++ atcoder1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atcoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+            int a = Convert.ToInt16(inputs[0]);
+            int b = Convert.ToInt16(inputs[1]);
+            int c = Convert.ToInt16(inputs[2]);
+           
+            if (c == 1)
+            {
+                while (a >=0 || b>= 0)
+                {
+                    b = b - 1;
+                   
+                    if (b == -1)
+                    {
+                        Console.WriteLine("Takahashi");
+                        break;
+                    }
+                      a = a - 1;
+                    if (a == -1)
+                    {
+                        Console.WriteLine("Aoki");
+                        break;
+                    }
+                    
+                }
+            }
+            else  if (c == 0)
+            {
+                while (a >= 0 || b >= 0)
+                {
+                    a = a- 1;
+                  
+                    if (a == -1)
+                    {
+                        Console.WriteLine("Aoki");
+                        break;
+                    }
+                    b = b - 1;
+                    if (b == -1)
+                    {
+                        Console.WriteLine("Takahashi");
+                        break;
+                    }
+                  
+                }
+            }
+
+        }
+    }
+}
