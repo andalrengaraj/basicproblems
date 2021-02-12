78863d88c1aa8e694b1a2b063beca6f1902d7c54
andalrengaraj <andalsanthini07@gmail.com>
2021-02-12 21:26:36 +0530

    adding contest

--- atcoder1.cs
+++ atcoder1.cs
@@ -10,53 +10,9 @@ namespace atcoder
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string[] inputs = input.Split(' ');
-            int a = Convert.ToInt16(inputs[0]);
-            int b = Convert.ToInt16(inputs[1]);
-            int c = Convert.ToInt16(inputs[2]);
-           
-            if (c == 1)
-            {
-                while (a >=0 || b>= 0)
-                {
-                    b = b - 1;
-                   
-                    if (b == -1)
-                    {
-                        Console.WriteLine("Takahashi");
-                        break;
-                    }
-                      a = a - 1;
-                    if (a == -1)
-                    {
-                        Console.WriteLine("Aoki");
-                        break;
-                    }
-                    
-                }
-            }
-            else  if (c == 0)
-            {
-                while (a >= 0 || b >= 0)
-                {
-                    a = a- 1;
-                  
-                    if (a == -1)
-                    {
-                        Console.WriteLine("Aoki");
-                        break;
-                    }
-                    b = b - 1;
-                    if (b == -1)
-                    {
-                        Console.WriteLine("Takahashi");
-                        break;
-                    }
-                  
-                }
-            }
+            Console.WriteLine("HEllo World");
 
+            
         }
     }
 }
